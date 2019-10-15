3158cadbf0b381178cd24897f923a67de2812936
ieko <mendo931411.mou@gmail.com>
2019-10-16 02:29:06 +0900

    寿司を生成して並べる処理を実装

---
+++ Assets/MyAssets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    [SerializeField] SusiGetaManager susiGetaManager;
+    // Start is called before the first frame update
+    void Start () {
+        Variables.gameState = GameState.GAME;
+        susiGetaManager.OnStart ();
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        switch (Variables.gameState) {
+            case GameState.START:
+                break;
+            case GameState.GAME:
+                susiGetaManager.OnUpdate ();
+                break;
+            case GameState.RESULT:
+                break;
+            default:
+                break;
+        }
+
+    }
+}
---
+++ Assets/MyAssets/Scripts/SusiGetaManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SusiGetaManager : MonoBehaviour {
+
+    [SerializeField] SusiGetaController susiGetaPrefab;
+    SusiGetaController[] susis;
+
+    // Start is called before the first frame update
+    public void OnStart () {
+        SusigetaGenerator ();
+    }
+
+    // Update is called once per frame
+    public void OnUpdate () {
+
+    }
+
+    void SusigetaGenerator () {
+        susis = new SusiGetaController[6];
+        Vector2 pos = new Vector2 (-9.6f, 0.67f);
+        Vector2 offset = new Vector2 (3.2f, 0f);
+        for (int i = 0; i < susis.Length; i++) {
+            pos += offset;
+            susis[i] = Instantiate (
+                susiGetaPrefab,
+                pos,
+                Quaternion.identity,
+                transform);
+        }
+    }
+}
