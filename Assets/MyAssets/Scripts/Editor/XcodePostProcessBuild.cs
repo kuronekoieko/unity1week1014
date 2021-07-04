458b0a542837ba9c11512e682fb37cb35733bb30
家子智成 <>
2021-07-04 22:03:33 +0900

    UIRequiredDeviceCapabilitiesのバイナリアップロードエラー修正

---
+++ Assets/MyAssets/Scripts/Editor/XcodePostProcessBuild.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEditor.Callbacks;
+
+#if UNITY_IOS
+using UnityEditor.iOS.Xcode;
+
+public static class XcodePostProcessBuild
+{
+    [PostProcessBuild]
+    public static void OnPostProcessBuild(BuildTarget target, string path)
+    {
+        var plistPath = Path.Combine(path, "Info.plist");
+        var plist = new PlistDocument();
+        plist.ReadFromFile(plistPath);
+
+        // バイナリアップロード時にエラーが出るため
+        plist.root.values.Remove("UIRequiredDeviceCapabilities");
+
+        plist.WriteToFile(plistPath);
+
+    }
+}
+#endif
